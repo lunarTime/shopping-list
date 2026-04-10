53079478ab7153ce9a8984dafc762f49cb45a31e
Nick Tale <ena2000.11@gmail.com>
2026-04-10 23:21:28 +0500

    Enhance service worker registration

--- src/main.tsx
+++ src/main.tsx
@@ -4,7 +4,15 @@ import { registerSW } from 'virtual:pwa-register';
 import './index.css';
 import App from '@app/App';
 
-registerSW({ immediate: true });
+registerSW({
+    immediate: true,
+    onRegistered(r) {
+        console.log('SW Registered: ', r);
+    },
+    onRegisterError(error) {
+        console.log('SW Registration error: ', error);
+    },
+});
 
 createRoot(document.getElementById('root')!).render(
     <StrictMode>
