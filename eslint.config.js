6f28c9e0d8699ffc75ff3b26f671b2066679b296
Nick Tale <ena2000.11@gmail.com>
2026-04-10 19:06:53 +0500

    Refactor ESLint configuration

--- eslint.config.js
+++ eslint.config.js
@@ -1,23 +1,60 @@
-import js from '@eslint/js'
-import globals from 'globals'
-import reactHooks from 'eslint-plugin-react-hooks'
-import reactRefresh from 'eslint-plugin-react-refresh'
-import tseslint from 'typescript-eslint'
-import { defineConfig, globalIgnores } from 'eslint/config'
+import js from '@eslint/js';
+import tseslint from 'typescript-eslint';
+import react from 'eslint-plugin-react';
+import reactHooks from 'eslint-plugin-react-hooks';
+import importPlugin from 'eslint-plugin-import';
+import unusedImports from 'eslint-plugin-unused-imports';
+import prettier from 'eslint-config-prettier';
 
-export default defineConfig([
-  globalIgnores(['dist']),
-  {
-    files: ['**/*.{ts,tsx}'],
-    extends: [
-      js.configs.recommended,
-      tseslint.configs.recommended,
-      reactHooks.configs.flat.recommended,
-      reactRefresh.configs.vite,
-    ],
-    languageOptions: {
-      ecmaVersion: 2020,
-      globals: globals.browser,
+export default [
+    js.configs.recommended,
+    ...tseslint.configs.recommended,
+    {
+        files: ['**/*.{jsx,tsx}'],
+        plugins: {
+            react,
+            'react-hooks': reactHooks,
+            import: importPlugin,
+            'unused-imports': unusedImports,
+        },
+        languageOptions: {
+            parserOptions: {
+                ecmaVersion: 'latest',
+                sourceType: 'module',
+            },
+        },
+        rules: {
+            curly: ['error', 'all'],
+            'padding-line-between-statements': [
+                'warn',
+                { blankLine: 'always', prev: '*', next: 'return' },
+                { blankLine: 'always', prev: 'const', next: 'return' },
+                { blankLine: 'always', prev: '*', next: 'if' },
+                { blankLine: 'always', prev: 'if', next: '*' },
+                { blankLine: 'always', prev: '*', next: 'return' },
+                { blankLine: 'always', prev: 'const', next: 'return' },
+            ],
+            'unused-imports/no-unused-imports': 'error',
+            'unused-imports/no-unused-vars': ['warn', { argsIgnorePattern: '^_' }],
+            'react/react-in-jsx-scope': 'off',
+            'react-hooks/rules-of-hooks': 'error',
+            'react-hooks/exhaustive-deps': 'warn',
+            'import/order': [
+                'warn',
+                {
+                    groups: ['builtin', 'external', 'internal'],
+                    'newlines-between': 'always',
+                },
+            ],
+            'no-console': 'warn',
+            'no-debugger': 'error',
+        },
+        settings: {
+            react: {
+                version: 'detect',
+            },
+        },
     },
-  },
-])
+
+    prettier,
+];
